aa338f3862cd5d4138463b1326658c780f6df15a
吕富明 <yanchifeng@users.noreply.github.com>
2016-06-30 13:29:58 +0800

    Create acount good.cpp

---
+++ acount good.cpp
@@ -0,0 +1,19 @@
+
+#include"good.h"
+#include<iostream>
+using namespace std;
+string acountGood::getName() {
+	return name;
+}
+string acountGood::getUnit() {
+	return unit;
+}
+float acountGood::getPrice() {
+	return price;
+}
+int acountGood::getCount() {
+	return count;
+}
+float acountGood::getDiscount() {
+	return discount;
+}
